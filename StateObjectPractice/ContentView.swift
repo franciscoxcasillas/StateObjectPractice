b4f165f16ae83bd65b3ca34805cb11fec215cbb1
Francisco Casillas <chiccasillas@gmail.com>
2025-03-06 12:29:33 -0600

    Create a todo list using ObservableObject and StateObject

--- StateObjectPractice/ContentView.swift
+++ StateObjectPractice/ContentView.swift
@@ -7,15 +7,62 @@
 
 import SwiftUI
 
+class TaskViewModel: ObservableObject {
+    @Published var tasks: [String] = []
+    
+    func addTask(_ task: String) {
+        tasks.append(task)
+    }
+    
+    func removeTask(at index: Int) {
+        tasks.remove(at: index)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
+        TaskListView()
+    }
+}
+
+struct TaskListView : View {
+    
+    @StateObject private var viewModel = TaskViewModel()
+    @State private var newTask: String = ""
+    @State private var isStrikethrough: Bool = false
+    
+    var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                TextField("Add task", text: $newTask)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 250)
+                
+                Button(action: {
+                    if !newTask.isEmpty {
+                        viewModel.addTask(newTask)
+                        newTask = ""
+                        print(viewModel.tasks)
+                    }
+                }) {
+                    Image(systemName: "plus")   
+                }
+            }
+            
+            VStack() {
+                ForEach(0..<viewModel.tasks.count, id: \.self) { index in
+                    HStack {
+                        Text(self.viewModel.tasks[index])
+                        Button(action: {
+                            self.viewModel.removeTask(at: index)
+                            print(viewModel.tasks)
+                        }) {
+                            Image(systemName: "xmark")
+                        }
+                    }
+                                        }
+            }
         }
-        .padding()
     }
 }
 
